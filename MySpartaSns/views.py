7d5615d365a95ccebc1eb3eb27c7f98829919993
gracegoh <gracegoh924@gmail.com>
2022-09-27 00:21:04 +0900

    django를 이용해 페이지 만들기

---
+++ MySpartaSns/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render # render: html 보여주는 역할
+
+
+def base_response(request):
+    return HttpResponse("안녕하세요! 대랑이 여자칭구입니다!")
+
+def first_view(request): # first_view() 함수는 'my_test.html' 보여주는 역할
+    return render(request, 'my_test.html')
+
+# 함수를 만들었으니 url과 연동해야. > urls.py
